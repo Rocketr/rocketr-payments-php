9bedb1db22c274b7128d7abab02fb00822130482
saadtronics <saadtronics@live.com>
2018-02-28 10:44:28 -0600

    Separate classes into individual files

--- lib/RocketrPaymentsException.php
+++ lib/RocketrPaymentsException.php
@@ -10,24 +10,4 @@ class RocketrPaymentsException extends \Exception {
     }
 }
 
-class RocketrPaymentsApiException extends \Exception {
-
-	protected $httpErrorCode;
-	protected $httpErrorArray;
-
-	public function __construct($message, $httpErrorCode, $code = 0, Exception $previous = null) {
-		$this->httpErrorCode = $httpErrorCode;
-		$this->httpErrorArray = $message;
-		if(is_array($message))
-			$message = $message['error'];
-
-		parent::__construct($message, $code, $previous);
-    }
-
-    public function getCompleteErrorArray() {
-    	return $this->httpErrorArray();
-    }
-
-}
-
 ?>
